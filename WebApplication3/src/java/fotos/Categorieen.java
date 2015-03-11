52f2d376670118f3512d7545660ea04adb44e552
ctsmulders <coen.smulders@student.fontys.nl>
2015-03-11 16:02:18 +0100

    categorieen

--- WebApplication3/src/java/fotos/Categorieen.java
+++ WebApplication3/src/java/fotos/Categorieen.java
@@ -24,6 +24,24 @@ public class Categorieen {
     public List<String> GetAllCategories(){
         List<String> Categories = new ArrayList<>();
         //ToDo: alle categorieën uit de database ophalen
+        //ToDo: code in commentaar controleren
+        //throws SQLException {
+        //  String Query = "SELECT * FROM *"
+        //  try {
+        //      stmt = con.createStatement();
+        //      ResultSet rs = stmt.executeQuery;
+        //      while (rs.next()){
+        //          String catName = rs.getString("");
+        //          Categories.add(catName);
+        //      }
+        //  }
+        //  catch (SQLException e ){
+        //      JDBCTutorialUtilities.printSQLException(e);
+        //  }
+        //  finally{
+        //      if (stmt != null) { stmt.close();
+        //  }
+        //}
         //dummydata:
         Categories.add("DummyCat");
         Categories.add("NewDummy");
