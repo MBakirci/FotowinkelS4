6dbba7f7b4606dfd9e1a7f4c228be2e7ed55aa3e
ctsmulders <coen.smulders@student.fontys.nl>
2015-03-11 13:30:05 +0100

    2 methodes in categorieen toegevoegd

--- WebApplication3/src/java/fotos/Categorieen.java
+++ WebApplication3/src/java/fotos/Categorieen.java
@@ -5,10 +5,38 @@
  */
 package fotos;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Coen
  */
 public class Categorieen {
+    //huidige categorie
+    String CurrentCatogory;
+    public Categorieen(){
+        
+    }
     
+    //load all categories form database
+    public List<String> GetAllCategories(){
+        List<String> Categories = new ArrayList<>();
+        //ToDo: alle categorieën uit de database ophalen
+        //dummydata:
+        Categories.add("DummyCat");
+        Categories.add("NewDummy");
+        Categories.add("TestDummy");
+        Categories.add("DummyDoll");
+        Categories.add("LaDummy");
+        Categories.add("LastDummy");
+        Categories.add("DummyBase");
+        Categories.add("LordDummy");
+        return Categories;
+    }
+    
+    //set CurrentCategrorie
+    public void SetCategory(String Categorie){
+        this.CurrentCatogory = Categorie;
+    }
 }
