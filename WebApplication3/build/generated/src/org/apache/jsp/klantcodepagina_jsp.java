044158df3c30b21d62a1f60668bcb928794595d0
JeffKain <Heartless275>
2015-05-15 13:27:23 +0200

    Revert "Klantcodepagina"
    This reverts commit 9259a004e2c4cd16bebb00437adb1816722fc5d0.

--- WebApplication3/build/generated/src/org/apache/jsp/klantcodepagina_jsp.java
+++ WebApplication3/build/generated/src/org/apache/jsp/klantcodepagina_jsp.java
@@ -3,9 +3,6 @@ package org.apache.jsp;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.jsp.*;
-import oracle.net.aso.a;
-import Test.Foto_klant;
-import Test.Photo;
 
 public final class klantcodepagina_jsp extends org.apache.jasper.runtime.HttpJspBase
     implements org.apache.jasper.runtime.JspSourceDependent {
@@ -46,33 +43,15 @@ public final class klantcodepagina_jsp extends org.apache.jasper.runtime.HttpJsp
 
       out.write("\r\n");
       out.write("\r\n");
-      out.write("<!-- \r\n");
-      out.write("    Document   : klantcodepagina\r\n");
-      out.write("    Created on : 8-apr-2015, 11:11:12\r\n");
-      out.write("    Author     : hsm\r\n");
-      out.write("-->\r\n");
-      out.write("\r\n");
-      out.write("\r\n");
-      out.write("\r\n");
       out.write("\r\n");
       out.write("<!DOCTYPE html>\r\n");
       out.write("<html>\r\n");
       out.write("    <head>\r\n");
       out.write("        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\r\n");
       out.write("        <title>Klant Pagina</title>\r\n");
-      out.write("                <!-- Bootstrap core CSS -->\r\n");
-      out.write("        <link href=\"CSS/bootstrap.min.css\" rel=\"stylesheet\" type=\"text/css\"/>\r\n");
-      out.write("\r\n");
-      out.write("        <!-- Custom styles for this template -->\r\n");
-      out.write("        <link href=\"CSS/justified-nav.css\" rel=\"stylesheet\">\r\n");
-      out.write("\r\n");
-      out.write("        <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->\r\n");
-      out.write("        <!--[if lt IE 9]><script src=\"../../assets/js/ie8-responsive-file-warning.js\"></script><![endif]-->\r\n");
-      out.write("        <script src=\"js/ie-emulation-modes-warning.js\"></script>\r\n");
       out.write("    </head>\r\n");
       out.write("    <body>\r\n");
-      out.write("        <form>\r\n");
-      out.write("\r\n");
+      out.write("        \r\n");
       out.write("            <div class=\"masthead\">\r\n");
       out.write("                <h3 class=\"text-muted\">Project name</h3>\r\n");
       out.write("                <nav>\r\n");
@@ -82,81 +61,57 @@ public final class klantcodepagina_jsp extends org.apache.jasper.runtime.HttpJsp
       out.write("                        <li><a href=\"fotograafProfiel.jsp\">Profiel</a></li>\r\n");
       out.write("                        <li><a href=\"Registreren.jsp\">Registreren</a></li>\r\n");
       out.write("                        <li><a href=\"Inlogscherm.jsp\">login</a></li>\r\n");
-      out.write("                        <!--<li><a href=\"logout.jsp\">Logout</a></li>-->\r\n");
-      out.write("                        <!--<li><a href=\"Upload.jsp\">Upload</a></li>-->\r\n");
+      out.write("                        <li><a href=\"logout.jsp\">Logout</a></li>\r\n");
+      out.write("                        <li><a href=\"Upload.jsp\">Upload</a></li>\r\n");
       out.write("                    </ul>\r\n");
       out.write("                </nav>\r\n");
       out.write("            </div>\r\n");
-      out.write("\r\n");
-      out.write("            <h1>Vult hier uw persoonlijke code voor de foto's in die u wilt bekijken</h1>\r\n");
-      out.write("\r\n");
-      out.write("            <label for=\"inputvoornaam\" class=\"sr-only\">Code</label>\r\n");
-      out.write("            ");
- if(session.getAttribute("Name")!=null)
-            {
-               out.print("U bent niet ingelogd!");
-            
-      out.write("\r\n");
-      out.write("           \r\n");
-      out.write("            <input type=\"text\" id=\"inputCode\" name=\"Code\" placeholder=\"Code\" required>\r\n");
-      out.write("\r\n");
-      out.write("            <button type=\"submit\" name=\"btnCode\">Bekijk de foto's</button>\r\n");
-      out.write("            ");
-
+      out.write("        \r\n");
+      out.write("        \r\n");
+      out.write("        <h1>Vult hier uw persoonlijke code voor de foto's in die u wilt bekijken</h1>\r\n");
+      out.write("        <form\r\n");
+      out.write("         <label for=\"inputvoornaam\" class=\"sr-only\">Code</label>\r\n");
+      out.write("        <input type=\"text\" id=\"inputCode\" name=\"Code\" class=\"form-control\" placeholder=\"Code\"required>\r\n");
+      out.write("        \r\n");
+      out.write("        <button class=\"btn btn-lg btn-primary btn-block\" type=\"submit\" name=\"btnCode\">Bekijk de foto's</button>\r\n");
+      out.write("        </form>\r\n");
+      out.write("        ");
  
-            }
-                
+            String code = request.getParameter("Code");
+            String error = "";
+            if(request.getParameter("btnCode") != null)
+                     {
+                        Test.Controleercode reg = new Test.Controleercode(code);
+                     
+                     if(!reg.Verbind()){
+                         error = "De code bestaat niet controleer deze en probeer het opnieuw";
+                        
+                          
+                         
+                         //response.addHeader("labelmislukt", "inloggen is mislukt");
+                     }
+                         //response.sendRedirect("Inlogscherm.jsp");}
+                         
+                     
+                         else {
+                                
+                                out.print("het is gelukt");
+                                response.sendRedirect("index.jsp");                     }
+                                
+                     }
+                 
+            
+       // if(request.getParameter("Code") != null)   
+       // {
+       //      response.sendRedirect("Inlogscherm.jsp");
+       // }
+        
       out.write("\r\n");
-      out.write("\r\n");
-      out.write("                ");
-
-                    String error = "";
-                    if (request.getParameter("btnCode") != null) {
-                    String code = request.getParameter("Code");
-  
-                    Test.Foto_klant FotoCode = new Test.Foto_klant();
-                    String src = FotoCode.VraagFoto(code);
-                    request.setAttribute("image", src);
-                               // Test.Controleercode reg = new Test.Controleercode(code);
-                        if (src == null) {
-                            error = "De code bestaat niet controleer deze en probeer het opnieuw";
-
-                            //response.addHeader("labelmislukt", "inloggen is mislukt");
-                        } //response.sendRedirect("Inlogscherm.jsp");}
-                        else {
-
-                            request.setAttribute("image", src);
-                            out.print("het is gelukt");
-                            Test.Photo idcode =  new Test.Photo();
-                            String a = idcode.getID(session.getAttribute("Name").toString());
-                            int b = Integer.parseInt(a);
-                            Test.SQL sql = new Test.SQL();
-                            sql.koppeling(b, code);
-                            
-                             
-                            
-                            
-                            //   response.sendRedirect("index.jsp");                     }
-
-                        }
-                    }
-               // if(request.getParameter("Code") != null)   
-                        // {
-                        //      response.sendRedirect("Inlogscherm.jsp");
-                // }
-
-      out.write("\r\n");
-      out.write("                <img src=\"");
-      out.print(request.getAttribute("image"));
-      out.write("\">\r\n");
-      out.write("                </form>\r\n");
-      out.write("\r\n");
-      out.write("                <font color=\"red\">");
+      out.write("        <font color=\"red\">");
       out.print(error);
       out.write("</font>\r\n");
-      out.write("\r\n");
-      out.write("                </body>\r\n");
-      out.write("                </html>\r\n");
+      out.write("    </body>\r\n");
+      out.write("</html>\r\n");
     } catch (Throwable t) {
       if (!(t instanceof SkipPageException)){
         out = _jspx_out;
--- WebApplication3/web/klantcodepagina.jsp
+++ WebApplication3/web/klantcodepagina.jsp
@@ -14,15 +14,6 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>Klant Pagina</title>
-                <!-- Bootstrap core CSS -->
-        <link href="CSS/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-
-        <!-- Custom styles for this template -->
-        <link href="CSS/justified-nav.css" rel="stylesheet">
-
-        <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-        <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-        <script src="js/ie-emulation-modes-warning.js"></script>
     </head>
     <body>
         <form>
@@ -36,8 +27,8 @@
                         <li><a href="fotograafProfiel.jsp">Profiel</a></li>
                         <li><a href="Registreren.jsp">Registreren</a></li>
                         <li><a href="Inlogscherm.jsp">login</a></li>
-                        <!--<li><a href="logout.jsp">Logout</a></li>-->
-                        <!--<li><a href="Upload.jsp">Upload</a></li>-->
+                        <li><a href="logout.jsp">Logout</a></li>
+                        <li><a href="Upload.jsp">Upload</a></li>
                     </ul>
                 </nav>
             </div>
@@ -45,24 +36,19 @@
             <h1>Vult hier uw persoonlijke code voor de foto's in die u wilt bekijken</h1>
 
             <label for="inputvoornaam" class="sr-only">Code</label>
-            <% if(session.getAttribute("Name")!=null)
-            {
-            %>
-           
-            <input type="text" id="inputCode" name="Code" placeholder="Code" required>
+            <input type="text" id="inputCode" name="Code" class="form-control" placeholder="Code" required>
 
-            <button type="submit" name="btnCode">Bekijk de foto's</button>
-           <%
-            }
-            %>
+            <button class="btn btn-lg btn-primary btn-block" type="submit" name="btnCode">Bekijk de foto's</button>
+            
+
                 <%
-                    String error = "";
-                    if (request.getParameter("btnCode") != null) {
                     String code = request.getParameter("Code");
-  
+                    String error = "";
                     Test.Foto_klant FotoCode = new Test.Foto_klant();
                     String src = FotoCode.VraagFoto(code);
                     request.setAttribute("image", src);
+                    if (request.getParameter("btnCode") != null) {
+
                                // Test.Controleercode reg = new Test.Controleercode(code);
                         if (src == null) {
                             error = "De code bestaat niet controleer deze en probeer het opnieuw";
@@ -92,7 +78,6 @@
                 // }
 %>
                 <img src="<%=request.getAttribute("image")%>">
-                
                 </form>
 
                 <font color="red"><%=error%></font>
